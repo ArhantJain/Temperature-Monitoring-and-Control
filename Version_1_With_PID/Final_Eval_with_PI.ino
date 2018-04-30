5660f480201b0830b51ffc45fb8749ae052c7e07
ArhantJain <arhant.code@gmail.com>
2018-04-30 15:13:27 +0530

    Commit: 2

--- Version_1_With_PID/Final_Eval_with_PI.ino
+++ Version_1_With_PID/Final_Eval_with_PI.ino
@@ -6,6 +6,14 @@ float tempF;
 int pwmfan1 = 6;
 int pwmfan2 = 9;
 
+////////////////Enter required Temperature/////////////////////
+//////////////////////////////////////////////////////
+float setTemp=40;
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+
+
+
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
@@ -14,7 +22,7 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 int pwmbulb = 10;
 double Setpoint, Input, Output,Output1;
-double Kp=2, Ki=1, Kd=0;
+double Kp=150, Ki=1, Kd=0;
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 PID myPID1(&Input, &Output1, &Setpoint, Kp, Ki, Kd, DIRECT);
 
@@ -32,7 +40,7 @@ int flag=0;
 int tl=20;
 int th=80;
 //int setTemp=(tl+th)/2;
-float setTemp=35;
+
 int logpin=A2;
 char rem = 0;
 String tmp;
@@ -50,11 +58,10 @@ void setup()
   pinMode(pwmbulb, OUTPUT);
   pinMode(logpin, INPUT);
   
- setPwmFrequency(pwmbulb, 1024);
- //setPwmFrequency(pwmfan, 1024);
   Serial.begin(9600); 
-   Setpoint = (setTemp+1)*(1024/110);
+   Setpoint = (setTemp)*(1023/110);
     myPID.SetMode(AUTOMATIC);
+    myPID1.SetMode(AUTOMATIC);
     
     lcd.begin(16, 2);
     lcd.setCursor(0,0);
@@ -92,44 +99,18 @@ void loop()
   //float tp = (5*Input*100/1024);
   Serial.println(tp); // Current Temperature
   lcd.setCursor(10,1);
-  lcd.print(tempC);
+  lcd.print(tp);
   lcd.println("C");
+  lcd.setCursor(10,0);
+  lcd.print(setTemp);
+  lcd.print("C");
   myPID.Compute();
   myPID1.Compute();
-  analogWrite(pwmfan1,Output);
-  analogWrite(pwmfan2,Output);
+  analogWrite(pwmfan1,Output1);
+  analogWrite(pwmfan2,Output1);
   analogWrite(pwmbulb, Output);
+  delay(200);
 }
   
   
-void setPwmFrequency(int pin, int divisor) {
-  byte mode;
-  if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
-    switch(divisor) {
-      case 1: mode = 0x01; break;
-      case 8: mode = 0x02; break;
-      case 64: mode = 0x03; break;
-      case 256: mode = 0x04; break;
-      case 1024: mode = 0x05; break;
-      default: return;
-    }
-    if(pin == 5 || pin == 6) {
-      TCCR0B = TCCR0B & 0b11111000 | mode;
-    } else {
-      TCCR1B = TCCR1B & 0b11111000 | mode;
-    }
-  } else if(pin == 3 || pin == 11) {
-    switch(divisor) {
-      case 1: mode = 0x01; break;
-      case 8: mode = 0x02; break;
-      case 32: mode = 0x03; break;
-      case 64: mode = 0x04; break;
-      case 128: mode = 0x05; break;
-      case 256: mode = 0x06; break;
-      case 1024: mode = 0x7; break;
-      default: return;
-    }
-    TCCR2B = TCCR2B & 0b11111000 | mode;
-  }
-}
 
